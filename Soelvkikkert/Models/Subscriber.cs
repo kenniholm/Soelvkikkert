2fc79ddbdc0fd21df776cfe280714bce3a15190d
Kasper <kasp724s@edu.ucl.dk>
2019-09-13 10:01:55 +0200

    make model for subscribers

---
+++ Soelvkikkert/Models/Subscriber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Soelvkikkert.Models
+{
+    public class Subscriber
+    {
+
+        private Subscriber()
+        {
+
+        }
+
+        private string Email { get; set; }
+        [Key]
+        private string PhoneNumber { get; set; }
+
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+
+        private bool active { get; set; }
+        private string product { get; set; }
+
+
+    }
+}
