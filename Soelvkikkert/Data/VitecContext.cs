b9cf5330768b6022bbbe299b043acafb428598ef
Molex <simplesleeptimer@gmail.com>
2019-09-17 11:01:58 +0200

    Add subsciber scaffold

--- Soelvkikkert/Data/VitecContext.cs
+++ Soelvkikkert/Data/VitecContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Soelvkikkert.Models;
 
 namespace Soelvkikkert.Models
 {
@@ -14,5 +15,7 @@ namespace Soelvkikkert.Models
         }
 
         public DbSet<Soelvkikkert.Models.Product> Product { get; set; }
+
+        public DbSet<Soelvkikkert.Models.Subscriber> Subscriber { get; set; }
     }
 }
