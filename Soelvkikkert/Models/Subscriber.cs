867a7d5caa6e7d3335b0b7bb14885baa204e079c
Molex <simplesleeptimer@gmail.com>
2019-09-20 11:15:15 +0200

    Add more validation to subscriber

--- Soelvkikkert/Models/Subscriber.cs
+++ Soelvkikkert/Models/Subscriber.cs
@@ -13,10 +13,21 @@ namespace Soelvkikkert.Models
         
         [Required]
         public string Email { get; set; }
+
+        [Display(Name = "Phone number")]
+        [RegularExpression(@"^[+]?\d{8,10}$")]
         public string PhoneNumber { get; set; }
+
         [Required]
+        // This regular expression should a name with a large start Peter but not peter
+        [RegularExpression(@"^([A-Z|Æ|Ø|Å]{1})([a-z|æ|ø|å])*$")]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+
         [Required]
+        // This regular expression should a name with a large start Peter but not peter
+        [RegularExpression(@"^([A-Z|Æ|Ø|Å]{1})([a-z|æ|ø|å])*$")]
+        [Display(Name = "Last name")]
         public string LastName { get; set; }
 
         public bool Active { get; set; }
