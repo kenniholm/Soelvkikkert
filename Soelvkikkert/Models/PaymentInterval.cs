74d79262ab270b59409d15a0694585e6533ffa21
Molex <simplesleeptimer@gmail.com>
2019-09-20 11:56:37 +0200

    Add column type

--- Soelvkikkert/Models/PaymentInterval.cs
+++ Soelvkikkert/Models/PaymentInterval.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,8 @@ namespace Soelvkikkert.Models
         public int ID { get; set; }
         [DataType(DataType.Time)]
         public TimeSpan Interval { get; set; }
+
+        [Column(TypeName = "decimal(18,4)")]
         public double Discount { get; set; }
     }
 }
--- Soelvkikkert/Models/Product.cs
+++ Soelvkikkert/Models/Product.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,6 +15,7 @@ namespace Soelvkikkert.Models
         public string Description { get; set; }
         [Required]
         [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
         [DataType(DataType.Date)]
         public DateTime SubscribtionStart { get; set; }
