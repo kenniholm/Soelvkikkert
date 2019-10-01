fad2ca90f951bcd9f1fd55b14c04f6fede23fb49
LAPTOP-4LGA03E1\kaspe <kasp724s@edu.ucl.dk>
2019-10-01 11:01:32 +0200

    Update VitecContex

--- SoelvkikkertMVC/Data/VitecContext.cs
+++ SoelvkikkertMVC/Data/VitecContext.cs
@@ -9,13 +9,28 @@ namespace SoelvkikkertMVC.Models
 {
     public class VitecContext : DbContext
     {
-        public VitecContext (DbContextOptions<VitecContext> options)
+        public VitecContext(DbContextOptions<VitecContext> options)
             : base(options)
         {
         }
 
         public DbSet<SoelvkikkertMVC.Models.Product> Product { get; set; }
-
         public DbSet<SoelvkikkertMVC.Models.Subscriber> Subscriber { get; set; }
+        public DbSet<SoelvkikkertMVC.Models.ProductPaymentInterval> ProductPaymentInterval { get; set; }
+        public DbSet<SoelvkikkertMVC.Models.PaymentInterval> PaymentInterval { get; set; }
+        public DbSet<SoelvkikkertMVC.Models.SubscriberProduct> SubscriberProduct { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().ToTable("Product");
+            modelBuilder.Entity<Subscriber>().ToTable("Subscriber");
+            modelBuilder.Entity<PaymentInterval>().ToTable("PaymentInterval");
+            modelBuilder.Entity<ProductPaymentInterval>().ToTable("ProductPaymentInterval");
+            modelBuilder.Entity<ProductPaymentInterval>()
+                .HasKey(p => new { p.PaymentIntervalID, p.ProductID });
+            modelBuilder.Entity<SubscriberProduct>().ToTable("SubscriberProduct");
+            modelBuilder.Entity<SubscriberProduct>()
+                .HasKey(p => new { p.SubscriberID, p.ProductID });
+        }
     }
 }
