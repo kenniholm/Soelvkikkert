111f90917e3577e708f39521fe2752e16144b624
LAPTOP-4LGA03E1\kaspe <kasp724s@edu.ucl.dk>
2019-10-01 10:55:18 +0200

    Scaffold product and subscriber

---
+++ SoelvkikkertMVC/Data/VitecContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SoelvkikkertMVC.Models;
+
+namespace SoelvkikkertMVC.Models
+{
+    public class VitecContext : DbContext
+    {
+        public VitecContext (DbContextOptions<VitecContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SoelvkikkertMVC.Models.Product> Product { get; set; }
+
+        public DbSet<SoelvkikkertMVC.Models.Subscriber> Subscriber { get; set; }
+    }
+}
