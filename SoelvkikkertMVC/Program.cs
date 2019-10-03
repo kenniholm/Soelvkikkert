893e428059c8f7169365d5560caf09264a5f56a1
Kasper Hoffmann <kasp724s@edu.ucl.dk>
2019-10-03 10:55:53 +0200

    Create seed data for employee and update main method

--- SoelvkikkertMVC/Program.cs
+++ SoelvkikkertMVC/Program.cs
@@ -15,7 +15,30 @@ namespace SoelvkikkertMVC
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<SchoolContext>();
+                    DbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
