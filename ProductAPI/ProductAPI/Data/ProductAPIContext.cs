628c3bd4d5c3d911e0fd513ca39469a53746ca32
Kenni <kens123@hotmail.dk>
2019-11-01 11:30:03 +0100

    Add API project with models and Controller

---
+++ ProductAPI/ProductAPI/Data/ProductAPIContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductAPI.Models
+{
+    public class ProductAPIContext : DbContext
+    {
+        public ProductAPIContext (DbContextOptions<ProductAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductAPI.Models.Product> Product { get; set; }
+    }
+}
