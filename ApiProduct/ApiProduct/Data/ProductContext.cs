80a3fda0468757b65633e9d95ddcfbc7acf87f13
Victor <vict536f@edu.ucl.dk>
2019-11-05 12:09:25 +0100

    Api done right

---
+++ ApiProduct/ApiProduct/Data/ProductContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ApiProduct.Models;
+
+namespace ApiProduct.Models
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext (DbContextOptions<ProductContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PaymentInterval> PaymentInterval { get; set; }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
