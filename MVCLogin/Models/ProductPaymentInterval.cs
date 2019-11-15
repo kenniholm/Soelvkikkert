1aa3f446ae5464f39d6b4490f81a893367fc6d41
Victor <vict536f@edu.ucl.dk>
2019-11-15 13:37:03 +0100

    Login
    Det virker næsten, men kan ikke magte mere :i <3

---
+++ MVCLogin/Models/ProductPaymentInterval.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCLogin.Models
+{
+    public class ProductPaymentInterval
+    {
+        public int PaymentIntervalID { get; set; }
+        public int ProductID { get; set; }
+    }
+}
---
+++ MVCLogin/Models/SubscriberProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCLogin.Models
+{
+    public class SubscriberProduct
+    {
+        public int ProductID { get; set; }
+        public int SubscriberID { get; set; }
+        public int PaymentIntervalID { get; set; }
+        public PaymentInterval SelectedPaymentInterval { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime SubscriptionStart { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime SubscriptionEnd { get; set; }
+    }
+}
