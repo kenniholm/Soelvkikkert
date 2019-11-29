da2deb7974bfb85ae223127cdf7d75a50e2de770
Molex <simplesleeptimer@gmail.com>
2019-11-29 12:16:52 +0100

    Add another test No longer create options in constructor, the database name needs to be different to reset the db

--- ControllerTests/SubsriberTest.cs
+++ ControllerTests/SubsriberTest.cs
@@ -15,16 +15,13 @@ namespace ControllerTests
     public class SubsriberTest
     {
         DbContextOptions<VitecContext> options;
-        public SubsriberTest()
-        {
-            options = new DbContextOptionsBuilder<VitecContext>()
-                .UseInMemoryDatabase(databaseName: "SubsriberDatabase").Options;
-        }
         
         [Fact]
         public async void Index_ReturnsAViewResult_WithListOfSubscribers()
         {
             // Arrange
+            options = new DbContextOptionsBuilder<VitecContext>()
+                .UseInMemoryDatabase(databaseName: "IndexSubsriberDatabase").Options;
             VitecContext context = new VitecContext(options);
             context.Subscriber.Add(new Subscriber
             {
@@ -47,14 +44,52 @@ namespace ControllerTests
             SubscriberController controller = new SubscriberController(context);
 
             // Act
-            var result = await controller.Index();
+            IActionResult result = await controller.Index();
 
             // Assert that it's a viewResult
-            var viewResult = Assert.IsType<ViewResult>(result);
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
             // Assert that the model returned is a list of subscribers
             List<Subscriber> model = Assert.IsAssignableFrom<List<Subscriber>>(viewResult.ViewData.Model);
             // Asser that there's 2 subscribers
             Assert.Equal(2, model.Count);
+        }
+
+        [Fact]
+        public async void Details_ReturnsRequested_Model()
+        {
+            // Arrange
+            options = new DbContextOptionsBuilder<VitecContext>()
+                .UseInMemoryDatabase(databaseName: "DetailsSubsriberDatabase").Options;
+            VitecContext context = new VitecContext(options);
+            context.Subscriber.Add(new Subscriber
+            {
+                FirstName = "Kenni",
+                LastName = "Bobber",
+                PhoneNumber = "88888888",
+                Active = true,
+                Email = "Kenni@bobber.dk",
+                ID = 1
+            });
+            context.Subscriber.Add(new Subscriber
+            {
+                FirstName = "Nidolaj",
+                LastName = "Molle",
+                PhoneNumber = "88888888",
+                Active = true,
+                Email = "Nidolaj@molle.dk",
+                ID = 2
+            });
+            context.SaveChanges();
+            SubscriberController controller = new SubscriberController(context);
+
+            // Act
+            IActionResult result = await controller.Details(2);
+
+            ViewResult viewResult = Assert.IsType<ViewResult>(result);
+            // Assert that it's a subscriber as model
+            Subscriber sub = Assert.IsAssignableFrom<Subscriber>(viewResult.ViewData.Model);
+            // Assert that it's the correct subsriber
+            Assert.Equal("Nidolaj", sub.FirstName);
         }
     }
 }
